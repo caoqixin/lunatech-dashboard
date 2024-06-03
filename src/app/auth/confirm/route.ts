9e380d90b739d3eff357a90724021d79b80d2638
xinge <caoqixin@gmail.com>
2024-06-03 12:04:42 +0200

    fix auth/confirm

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,7 +1,7 @@
 import { type EmailOtpType } from "@supabase/supabase-js";
 import { type NextRequest, NextResponse } from "next/server";
 
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/lib/supabase/server";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
